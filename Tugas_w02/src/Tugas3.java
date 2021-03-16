91a887ccd443ea7bbe000c442b2bd0077946fcec
yulianadewi18 <75105622+yulianadewi18@users.noreply.github.com>
2021-03-16 09:35:42 +0700

    tugas week2

---
+++ Tugas_w02/src/Tugas3.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Yuliana Dewi
+ */
+public class Tugas3 {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // pendeklarasian yang benar
+        boolean gameOver=false;
+        int students=50,classes=3;
+        double sales_tax;
+        short number1;
+        
+        // pendeklarasian yang salah
+        int 2beOrNot2be;
+        float price index;
+        double lastYear'sPrice;
+        long class;
+    }
+}
+    
