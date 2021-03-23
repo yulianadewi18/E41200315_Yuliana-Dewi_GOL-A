bc6db0ddde62676ff9d249d638fd8052b36ad2d5
yulianadewi18 <75105622+yulianadewi18@users.noreply.github.com>
2021-03-23 09:11:47 +0700

    tugas_w03

---
+++ tugas_w03/src/tugas_w03/Tugas1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas_w03;
+
+/**
+ *
+ * @author Yuliana Dewi
+ */
+public class Tugas1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String[] teks = {"Yuliana","Dewi"};
+        
+        for (int j=0; j<teks.length; j++) {
+            System.out.print(teks[j]);
+            System.out.print(" ");
+        }
+    }
+    
+}
