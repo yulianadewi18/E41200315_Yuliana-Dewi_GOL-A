c0a61da14bdd043c13571d57920310f2133eb40f
yulianadewi18 <75105622+yulianadewi18@users.noreply.github.com>
2021-03-30 08:55:48 +0700

    week04

--- tugas_w03/src/tugas_w03/Tugas1.java
+++ tugas_w03/src/tugas_w03/Tugas1.java
@@ -14,14 +14,17 @@ public class Tugas1 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         // TODO code application logic here
-        String[] teks = {"Yuliana","Dewi"};
-        
-        for (int j=0; j<teks.length; j++) {
-            System.out.print(teks[j]);
-            System.out.print(" ");
-        }
+        System.out.println ("         Do__While");
+            System.out.println ("Bilangan Kelipatan 2 (1-100)");
+            System.out.println ("=================================");
+        int i = 1;
+        do {
+            System.out.print(i*2 + " " );
+            i++;
+        } 
+        while (i <=50);
     }
-    
 }
+    
