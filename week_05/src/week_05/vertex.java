599266e0fd0a16ab6cbdcd03ef0ed9e8ff4eef8d
yulianadewi18 <75105622+yulianadewi18@users.noreply.github.com>
2021-04-10 00:45:42 +0700

    week05

---
+++ week_05/src/week_05/vertex.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week_05;
+
+/**
+ *
+ * @author Yuliana Dewi
+ */
+public class vertex {
+    public char label;
+    public boolean wasVisited;
+    
+    public vertex(char label) {
+        this.label = label; wasVisited = false;
+        
+    }
+}
