cf92bba8f45c1f2619ce2b69baa9a0196914f397
yulianadewi18 <75105622+yulianadewi18@users.noreply.github.com>
2021-04-14 15:50:33 +0700

    WEEK02

---
+++ Tugas_w02/src/tugas_w02/Tugas1.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas_w02;
+
+/**
+ *
+ * @author Yuliana Dewi
+ */
+public class Tugas1 {
+
+    /**
+     * @param args the command line arguments
+     */
+        public static void main(String[] args) {
+        // TODO code application logic here
+        String fName = "Yuliana ";
+        String lName = "Dewi ";
+        int stuid = 41200315;
+        String stuStatus = "Active";
+
+        System.out.println("Student Name: " + fName +""+ lName);
+        System.out.println("Student ID: " + stuid);
+        System.out.println("Student Status: " + stuStatus);
+
+    }
+  }
---
+++ week02/src/week02/latihan2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week02;
+
+/**
+ *
+ * @author Yuliana Dewi
+ */
+public class latihan2 {
+    
+    public static void main(String[] args) {
+      
+    String nama = "Dewi";
+        char abjad = 'y';
+        int a = 18 ;
+        double b = 7.6;
+       
+        System.out.println (abjad);
+        System.out.println (nama);
+        
+    }
+        
+    
+}
