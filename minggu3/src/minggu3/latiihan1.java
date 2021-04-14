ba5e08f16c07230db554cf71c4322359181d9bc5
yulianadewi18 <75105622+yulianadewi18@users.noreply.github.com>
2021-04-14 15:49:34 +0700

    WEEK03

---
+++ minggu3/src/minggu3/latiihan1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minggu3;
+
+/**
+ *
+ * @author Yuliana Dewi
+ */
+public class latiihan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         int[] angka = {1, 3, 5, 7, 12, 21, 37};
+        
+        System.out.println("jumlah elemen array angka =" +angka.length);
+    }
+    
+}
---
+++ week03/src/week03/latihan1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week03;
+
+/**
+ *
+ * @author Yuliana Dewi
+ */
+public class latihan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[] angka = {1, 3, 5, 7, 12, 21, 37};
+        
+        System.out.println("jumlah elemen array angka =" +angka.length);
+    }
+    
+}
